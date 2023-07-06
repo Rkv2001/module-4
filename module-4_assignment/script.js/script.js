d5adfab6fe952944ff7c32ac8cb0da795afc280f
Rkv2001 <101959850+Rkv2001@users.noreply.github.com>
2023-07-06 12:51:28 +0530

    Update script.js

--- module-4_assignment/script.js/script.js
+++ module-4_assignment/script.js/script.js
@@ -2,7 +2,7 @@
   
    WARNING!!! WARNING!!!
 
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Rajat", "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i = 0; i < names.length; i++) {
 
